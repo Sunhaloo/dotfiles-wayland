1d394c9456c54f019916fa054dba11e7b5cdfd00
Sunhaloo <azmaan151003@gmail.com>
2024-11-12 12:35:30 +0400

    I think I have completed the Git Configuration script

--- wayland_script/git.sh
+++ wayland_script/git.sh
@@ -3,8 +3,9 @@
 # function to display the options to the user
 display_options() {
 	# output options to user
-	printf "\nOption[1]: Install Git"
-	printf "\nOption[2: Git Configuration\n"
+	printf "\nOption [1]: Install Git"
+	printf "\nOption [2]: Git Configuration"
+	printf "\nOption [3]: Exit\n\n"
 }
 
 
@@ -14,9 +15,8 @@ check_git_installed() {
 	printf "\nChecking if Git is installed...\n\n"
 
 	# conditions to evaluate based if Git is installed
-	if which git &> /dev/null
+	if which git &> /dev/null; then
 	# if Git is installed
-	then
 		printf "Git is already installed\n"
 	# if Git is NOT installed
 	else
@@ -27,9 +27,57 @@ check_git_installed() {
 }
 
 
+# create a function to configure Git
+configure_git() {
+	printf "\n"
+	# prompt the user to enter credentials
+	read -p "Please Enter Your Email ( attached with GitHub ): " git_email
+	read -p "Please Enter Your Username: " git_username
+
+	# start configuring Git ---> output appropriate message
+	printf "\nSetting Up Git...\n\n"
+
+	# configure git
+	git config --global user.email "$git_email"
+	git config --global user.name "$git_username"
+	git config --global init.defaultBranch main
+
+	# list Git configuration
+	git config --list
+
+	# output '-' 50 times
+	printf '%0.s-' {1..50}
+	printf "\n"
+
+	# output appropriate message
+	printf "\n\nGit Configuration Completed\n"
+}
+
+
 # out main function
 main () {
+	# call the function to display the options to the user
 	display_options
+	
+	# ask the user to select and option
+	read -p "Please Select and Option: " user_option
+
+	# output '-' 50 times
+	printf '%0.s-' {1..50}
+	printf "\n"
+
+	# conditions to evaluate depending on user's input
+	if [[ "$user_option" = 1 ]]; then
+		# call the function to check for Git in system
+		check_git_installed
+	elif [[ "$user_option" = 2 ]]; then
+		# call the function to configure Git
+		configure_git
+	elif [[ "$user_option" = 3 ]]; then
+		# user wants to exit the script
+		printf "\nGood Bye\n"
+		exit 0
+	fi
 }
 
 

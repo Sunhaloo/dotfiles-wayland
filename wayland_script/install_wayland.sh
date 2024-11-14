a4efb805b1077440e4c32548314d4ce7ef6fc848
Sunhaloo <azmaan151003@gmail.com>
2024-11-14 21:10:06 +0400

    I will continue tomorrow

--- wayland_script/install_wayland.sh
+++ wayland_script/install_wayland.sh
@@ -11,7 +11,7 @@ display_options() {
     # display the options
     printf "\nOption [1]: Update - Install Hyprland + Drivers + Tools"
 	printf "\nOption [2]: Update - Install GUI Applications"
-	printf "\nOption [3]: Move Configuration Files and Folders"
+	printf "\nOption [3]: Populate Folders and Files"
 	printf "\nOption [4]: Update - Install Terminal Applications"
     printf "\nOption [5]: Create - Move Folders"
     printf "\nOption [6]: GitHub Configuration"
@@ -60,35 +60,6 @@ install_yay_package() {
 }
 
 
-# function to evaluate user's choice on option
-select_choice_option() {
-	# conditions to evaluate depending on user's input
-	if [[ "$user_option" = 1 ]]; then
-		# call the function `install_hyprland_tools`
-		install_hyprland_tools
-
-	elif [[ "$user_option" = 2 ]]; then
-		# call the function `install_gui_apps`
-		install_gui_apps
-
-	elif [[ "$user_option" = 7 ]]; then
-		# user wants to exit the script
-		printf "\nGood Bye\n"
-		exit 0
-	else:
-		# users presses unavailable option
-		# output '-' 50 times
-		printf '%0.s-' {1..50}
-		printf "\n"
-
-		# output approriate message
-		printf "\nWrong Option\n"
-
-		# output '-' 50 times
-		printf '%0.s-' {1..50}
-		printf "\n"
-	fi
-}
 
 
 # function to update, install Hyprland, tools and drivers
@@ -108,7 +79,7 @@ install_hyprland_tools() {
 	printf '%0.s-' {1..50}
 	printf "\n\n"
 
-	# call the install function
+	# installing packages with pacman ( calling function `install_package` )
 	install_package git hyprland kitty hyprutils hyprshot hyprpicker hyprcursor hyprpaper hyprwayland-scanner xdg-desktop-portal-hyprland xdg-desktop-portal-wlr wl-clipboard power-profiles-daemon wl-clipboard power-profiles-daemon thunar-volman gvfs gvfs-afc brightnessctl gnome-keyring network-manager-applet ldns blueman lxinput rofi
 
 }
@@ -131,9 +102,64 @@ install_gui_apps() {
 	printf '%0.s-' {1..50}
 	printf "\n\n"
 
+	# installing packages with pacman ( calling function `install_package` )
 	install_package kitty obsidian obs-studio discord ristretto mpv
+	# installing packages with AUR helper ( calling function `install_yay_package` )
 	install_yay_package vscodium-bin thorium-browser-bin
 
+}
+
+
+# function to move configuration file from github repo
+move_all_configurations() {
+	printf "\nPopulating Folders and Files\n\n"
+
+	# output '-' 50 times
+	printf '%0.s-' {1..50}
+	printf "\n"
+
+	# make folders found in home directory
+	mkdir -p ~/{OBS\ Studio,Screenshots,Obsidian,GitHub}
+
+	# move all of the configuration files and folders to required location
+	# hyprland related configurations
+
+}
+
+
+# function to evaluate user's choice on option
+select_choice_option() {
+	# conditions to evaluate depending on user's input
+	if [[ "$user_option" = 1 ]]; then
+		# call the function `install_hyprland_tools`
+		install_hyprland_tools
+
+	elif [[ "$user_option" = 2 ]]; then
+		# call the function `install_gui_apps`
+		install_gui_apps
+
+	elif [[ "$user_option" = 3 ]]; then
+		# call function to move configuration files
+		move_all_configurations
+
+	elif [[ "$user_option" = 7 ]]; then
+		# user wants to exit the script
+		printf "\nGood Bye\n"
+		exit 0
+
+	else:
+		# users presses unavailable option
+		# output '-' 50 times
+		printf '%0.s-' {1..50}
+		printf "\n"
+
+		# output approriate message
+		printf "\nWrong Option\n"
+
+		# output '-' 50 times
+		printf '%0.s-' {1..50}
+		printf "\n"
+	fi
 }
 
 
@@ -141,7 +167,6 @@ install_gui_apps() {
 main() {
 	# call the function to display options to the user
 	display_options
-
 
 	# ask the user to select and option
 	read -p "Please Select and Option: " user_option
